6e5e846a83b8869d242bd3073227ece47df90c81
Jiwon Hur <jiwonhur6@gmail.com>
2023-05-24 15:18:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 require('dotenv').config()
 
-// API_KEY for maps api
+// get API key from envrioment variable
 const API_KEY = process.env.API_KEY;
 
-// get 
+// assigning references to HTML elements
 const searchButton = document.getElementById('searchButton')
-const cityName = document.getElementById('cityName')
+// const cityName = document.getElementById('cityName')
 const weatherDescription = document.getElementById('weatherDescription')
 const weatherInformation = document.getElementById('weatherInformation')
 const weatherImage = document.getElementById('weatherImage')
 const temp = document.getElementById('temp')
 const cityAndCountry = document.getElementById('cityAndCountry')
+const cityInput = document.getElementById('cityInput')
 
 
+// fetch weather data from OpenWeatherMap API for given city
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
 
@@ -26,12 +28,15 @@ getWeatherData = (city) => {
   })
 }
 
+// displaying the weather data on the webpage
 const showWeatherData = (weatherData) => {
 
+  // extracting various weather information
   const description = `${weatherData.weather[0].main}`
   const cityTemp = `${Math.ceil(weatherData.main.temp)}\xB0`
-  const humidity = weatherData.main.humidity
+  
 
+  // display weather information titles and values
   const allWeatherInfoTitles = ['Current Temp: ', 'Min temp: ', 'Max temp: ']
   const allWeatherInfo = [weatherData.main.temp, weatherData.main.temp_min, weatherData.main.temp_max]
 
@@ -39,11 +44,14 @@ const showWeatherData = (weatherData) => {
     return [`${a} ${allWeatherInfo[i]}\xB0 &emsp; &emsp;`].join('')
   }).join(' ')
 
+
+  // URLs for different weather conditions
   const rainyGif = ['https://img.icons8.com/color/512/downpour--v1.png']
   const clearGif = ['https://cdn-icons-png.flaticon.com/512/831/831682.png']
   const cloudsGif = ['https://cdn-icons-png.flaticon.com/512/4834/4834559.png']
   const mistGif = ['https://static.thenounproject.com/png/1512648-200.png']
 
+  // displaying the images based on the weather description
   switch (description) {
     case 'Rain':
     case 'Drizzle':
@@ -61,16 +69,13 @@ const showWeatherData = (weatherData) => {
         break;
   }
   
+  // update the HTML elements with the weather data
   temp.innerHTML = cityTemp
-
   weatherDescription.innerHTML = description
-
   weatherInformation.innerHTML = result
-
   cityAndCountry.innerHTML = `${weatherData.name}, ${weatherData.sys.country}`
 
 }
 
-const cityInput = document.getElementById('cityInput')
-
+// event listener to search button to retrive weather data
 searchButton.onclick = () => getWeatherData(cityInput.value)
